7908af8f3aac40b152be039bc7876e269359e563
Nolan ELASSI <nolan.el-assi@etu.univ-amu.fr>
2023-02-10 17:19:49 +0100

    modif settings

--- manageSport/settings.py
+++ manageSport/settings.py
@@ -124,9 +124,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = '/worldCupShop/static/'
+STATIC_URL = 'static/'
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static")
+    os.path.join(BASE_DIR, "worldCupShop/static"),
 ]
 
 
